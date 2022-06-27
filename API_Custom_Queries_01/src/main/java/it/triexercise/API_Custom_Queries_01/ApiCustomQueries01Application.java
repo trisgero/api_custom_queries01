e3129deeea94129c495bd501437f5883cd30950d
Tristan Geronimo <tristanpgeronimo@gmail.com>
2022-06-27 17:44:58 +0200

    Add files via upload

---
+++ API_Custom_Queries_01/src/main/java/it/triexercise/API_Custom_Queries_01/ApiCustomQueries01Application.java
@@ -0,0 +1,13 @@
+package it.triexercise.API_Custom_Queries_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCustomQueries01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCustomQueries01Application.class, args);
+	}
+
+}
---
+++ API_Custom_Queries_01/src/test/java/it/triexercise/API_Custom_Queries_01/ApiCustomQueries01ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.triexercise.API_Custom_Queries_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCustomQueries01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
